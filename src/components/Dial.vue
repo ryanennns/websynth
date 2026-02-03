cfa44358cbe3766883483336dd27bdb01386b149
Ryan Enns <ryanennns@gmail.com>
2026-02-03 08:38:28 -0500

    init II

--- src/components/Dial.vue
+++ src/components/Dial.vue
@@ -1,12 +1,24 @@
 <template>
-
+  <input type="range" :min="min" :max="max" :step="step" v-model="range"/>
+  <p>{{range}}</p>
 </template>
 
 <script setup lang="ts">
+import {ref} from "vue";
+
 interface Props {
-  max: number;
   min: number;
+  max: number;
+  step: number;
 }
 
-defineProps<Props>()
+defineProps<Props>();
+
+const range = ref<number>(0);
 </script>
+
+<style>
+input {
+  writing-mode: vertical-lr;
+}
+</style>
