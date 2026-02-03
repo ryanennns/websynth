fbc8dd336d752bc2a275e00377dce8692cb8b2b8
Ryan Enns <ryanennns@gmail.com>
2026-02-03 18:32:59 -0500

    jesse, we need to cook

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <script setup lang="ts">
 import Dial from "./components/Dial.vue";
-import { computed, ref, watch } from "vue";
+import {ref, watch} from "vue";
 import DialBack from "./components/Dials/Default/DialBack.vue";
 import DialHead from "./components/Dials/Default/DialHead.vue";
 
 const foo = ref<number>(0);
-const bar = ref<number>(0);
-const see = ref<number>(0.5);
-
-const sineMulti = computed<number>(() => see.value * 3);
 
 let counter = 0;
+const sineMulti = ref<number>(0.5);
+watch(sineMulti, () => counter = 0)
+
 let on = ref<boolean>(false);
 const sin = () => {
   if (!on.value) {
@@ -21,8 +20,7 @@ const sin = () => {
 
   counter += 0.05;
 
-  foo.value = val * 0.5 + 0.5;
-  bar.value = val * 100;
+  foo.value = (val * 100) * 0.5 + 50;
 
   setTimeout(sin, 10);
 };
@@ -34,26 +32,30 @@ sin();
   <div class="flex flex-col gap-8">
     <div class="flex-col flex justify-center items-center">
       <Dial
+        :max="100"
+        :min="0"
         :step="0.01"
         :offset="30"
-        :default="0.5"
+        :default="0"
         :head="DialHead"
         :back="DialBack"
         v-model="foo"
         class="h-32 w-32"
       />
+      {{Math.round(foo)}}
       <button @click="() => (on = !on) && sin()">toggle</button>
     </div>
     <div class="flex-col flex justify-center items-center">
       <Dial
+        :max="3"
+        :min="0"
         :offset="30"
-        :default="0.5"
         :head="DialHead"
         :back="DialBack"
-        v-model="see"
+        v-model="sineMulti"
         class="h-32 w-32"
       />
-      <p>{{ sineMulti }}</p>
+      <p>{{ sineMulti.toFixed(2) }}</p>
       <p>Sine Multiplier</p>
     </div>
   </div>
--- src/components/Dial.vue
+++ src/components/Dial.vue
@@ -12,11 +12,13 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref, watch } from "vue";
+import {computed, onMounted, ref, watch} from "vue";
 import { clampBetween } from "../helpers/helpers.ts";
 
 interface Props {
   modelValue: number;
+  max: number;
+  min: number;
   step?: number;
   offset?: number;
   default?: number;
@@ -27,8 +29,9 @@ interface Props {
 const props = defineProps<Props>();
 
 const offset = computed<number>(() => props.offset ?? 0);
-const range = computed<number>(() => 360 - 2 * offset.value);
-const step = computed<number>(() => (props.step ?? 1));
+const angleRange = computed<number>(() => 360 - 2 * offset.value);
+const valueRange = computed<number>(() => props.max - props.min);
+const step = computed<number>(() => props.step ?? 1);
 
 const mouseYOnMouseDown = ref<number | undefined>(undefined);
 const valueOnMouseDown = ref<number>(0);
@@ -38,15 +41,12 @@ const dialHeadRotation = computed<number>(
   () => zeroValueRotation.value + degrees.value,
 );
 
-const degrees = ref<number>((props.default ?? 1) * range.value);
-const setDegrees = (val: number) =>
-  (degrees.value = clampBetween(
-    0,
-    range.value,
-    Math.ceil(val / step.value) * step.value,
-  ));
+const degrees = ref<number>((props.default ?? 0));
+const setDegrees = (val: number) => {
+  degrees.value = clampBetween(0, angleRange.value, val);
+};
 
-const normalized = computed<number>(() => degrees.value / range.value);
+const normalized = computed<number>(() => degrees.value / angleRange.value);
 
 const handleMouseDown = (event: any) => {
   mouseYOnMouseDown.value = event.clientY;
@@ -78,7 +78,13 @@ const handleMousewheel = (event: any) => {
 };
 
 const emit = defineEmits(["update:modelValue"]);
-watch(normalized, (newValue) => emit("update:modelValue", newValue));
+watch(normalized, (newValue) =>
+  emit("update:modelValue", newValue * valueRange.value),
+);
 
-watch(props, (newValue) => setDegrees(range.value * newValue.modelValue));
+watch(props, (newValue) =>
+  setDegrees((newValue.modelValue / props.max) * angleRange.value),
+);
+
+onMounted(() => emit("update:modelValue", normalized.value * valueRange.value))
 </script>
