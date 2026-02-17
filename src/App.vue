0db120dbb8bf88b2e85b3cdcea01c793ff923af5
Ryan Enns <ryanennns@gmail.com>
2026-02-17 16:05:25 -0500

    matrix v1

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,3 @@
-<script setup lang="ts">
-import Dial from "./components/Dial.vue";
-import DialBack from "./components/Dials/Default/DialBack.vue";
-import DialHead from "./components/Dials/Default/DialHead.vue";
-import Lfo from "./components/Lfo.vue";
-import { matrix } from "@/controllers.ts";
-</script>
-
 <template>
   <div class="flex flex-row gap-16">
     <div class="flex gap-8 flex-col">
@@ -49,6 +41,49 @@ import { matrix } from "@/controllers.ts";
         {{ Math.round(matrix[0]![2]!) }}
       </div>
     </div>
-    <Lfo v-model="matrix[0]![0]!" />
+    <Lfo v-model="matrix[lfoOutput.x]![lfoOutput.y]!" />
+  </div>
+  <div class="p-4">
+    <div class="flex gap-4">
+      <div class="flex flex-col">
+        <label for="x">X (0–100)</label>
+        <input
+          id="x"
+          type="number"
+          min="0"
+          max="100"
+          v-model.number="lfoOutput.x"
+          class="border px-2 py-1"
+        />
+      </div>
+
+      <div class="flex flex-col">
+        <label for="y">Y (0–100)</label>
+        <input
+          id="y"
+          type="number"
+          min="0"
+          max="100"
+          v-model.number="lfoOutput.y"
+          class="border px-2 py-1"
+        />
+      </div>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import Dial from "@/components/Dial.vue";
+import DialBack from "@/components/Dials/Default/DialBack.vue";
+import DialHead from "@/components/Dials/Default/DialHead.vue";
+import Lfo from "@/components/Lfo.vue";
+import { matrix } from "@/controllers.ts";
+import { ref } from "vue";
+
+interface CoordinateSet {
+  x: number;
+  y: number;
+}
+
+const lfoOutput = ref<CoordinateSet>({ x: 0, y: 0 });
+</script>
